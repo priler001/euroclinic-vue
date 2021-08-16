1fc2cdfc16f581d919ce8447e8805761e65e6148
Yusuf <priler001@gmail.com>
2021-08-16 10:52:12 +0300

    commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+          <!-- Default footer-->
+      <footer class="section-relative section-34 section-md-66 page-footer bg-lighter context-light novi-background bg-cover">
+        <div class="shell-wide">
+          <div class="inset-xl-left-30">
+            <div class="range range-sm-center range-fix">
+              <div class="cell-sm-8 cell-md-12">
+                <div class="range range-md-justify range-md-middle range-fix">
+                  <div class="cell-md-4 cell-md-push-1 text-md-left">
+                    <!-- Footer brand-->
+                    <div class="footer-brand reveal-inline-block"><a href="index.html"><img src="images/евроклиник.svg" alt="" width="265" height="62"/></a></div>
+                  </div>
+                  <div class="cell-md-4 offset-top-55 offset-md-top-0 cell-md-push-3 text-md-right">
+                          <ul class="list-inline">
+                            <li><a class="icon novi-icon fa fa-facebook icon-xxs icon-circle icon-gray-light-2" href="#"></a></li>
+                            <li><a class="icon novi-icon fa fa-twitter icon-xxs icon-circle icon-gray-light-2" href="#"></a></li>
+                            <li><a target="_blank" class="icon novi-icon fa fa-whatsapp icon-xxs icon-circle icon-gray-light-2" href="https://api.whatsapp.com/send/?phone=996997911000&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%21%0A%0A%D0%9F%D0%B8%D1%88%D1%83+%D0%B8%D0%B7+%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F+2%D0%93%D0%98%D0%A1.%0A%0A&app_absent=0"></a></li>
+                            <li><a target="_blank" class="icon novi-icon fa fa-instagram icon-xxs icon-circle icon-gray-light-2" href="https://www.instagram.com/euroclinic.osh/"></a></li>
+                          </ul>
+                  </div>
+                  <div class="cell-md-4 offset-top-55 offset-md-top-0 cell-md-push-2">
+                    <p class="text-gray">&copy; <span class="copyright-year"></span> Все права защищены. <a class="text-gray" href="privacy-policy.html">Политика Конфиденциальности</a>
+                      
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'font-awesome/css/font-awesome.min.css'
+import 'jquery/dist/jquery.slim.js'
+
 createApp(App).use(router).mount('#app')
